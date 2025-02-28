dcc92be8c0a79863440e83189e660e03e8a5e5fa
sSuUjJaAlL <sujal.20207015@nami.edu.np>
2025-02-28 19:56:03 +0545

    changes done

--- src/controller/quote.controller.js
+++ src/controller/quote.controller.js
@@ -3,7 +3,10 @@ import { quoteService } from "../service/quote.service.js";
 async function quoteController(req,res){
     try{
         const body = req.body
-        const is_admin = req.user.is_admin;
+        const {is_admin} = req.user;
+        
+    
+
         const qservice= await quoteService(body,is_admin)
         return res.json({
             message: "Quote posted ",
